74ab0331567830f63c8dddeda424a35b8e334e70
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-05-08 00:09:23 +0600

    Js loop and function

---
+++ script.js
@@ -0,0 +1,57 @@
+// let marks = [50, 40, 98, 99];
+
+// for(i=0; i<marks.length; i++){
+//     console.log(marks[i]);
+// }
+
+// let i=0
+// while(i<marks.length){
+//     console.log(marks[i]);
+//     i++
+// }
+
+// let j = 1;
+
+// while(j<=5){
+//     console.log('Hello World!');
+//     j++;
+// }
+
+// let k = 0;
+
+// do{
+//     console.log(marks[k]);
+//     k++;
+// }
+
+// while(k<marks.length);
+
+// marks.forEach(element=>{
+//     console.log(element);
+// })
+
+// function show(){
+//     let marks = [50, 40, 98, 99];
+
+//     for(i=0; i<marks.length; i++){
+//         console.log(marks[i]);
+//     }
+// }
+
+function sum(number1, number2, number3){
+    // let x = number1;
+    // let y = number2;
+    // let z = number3;
+
+    // let result = x+y+z;
+    let result = number1+number2+number3;
+
+    return result;
+    // console.log(result);
+}
+
+let returnedValue = sum(30,40,70); //140
+
+let secondSummation = returnedValue + 150;
+
+console.log(secondSummation);
